ec4e75b28c1d9cb61034c6dd5262cd81b7a2f3f2
Rafael Choinhet <70548615+rxonhe@users.noreply.github.com>
2022-10-03 11:56:12 -0300

    adding input validation

--- src/main/java/com/choinhet/televisionproductionline/controller/BaseController.java
+++ src/main/java/com/choinhet/televisionproductionline/controller/BaseController.java
@@ -4,9 +4,12 @@ import com.choinhet.televisionproductionline.model.Television;
 import com.choinhet.televisionproductionline.service.BaseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -32,7 +35,10 @@ public class BaseController {
     }
 
     @PostMapping("save/television")
-    public String saveTelevision(Television television) {
+    public String saveTelevision(@Valid @ModelAttribute("television") Television television, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "new-television";
+        }
         baseService.save(television);
         return "redirect:/televisions";
     }
--- src/main/java/com/choinhet/televisionproductionline/model/Television.java
+++ src/main/java/com/choinhet/televisionproductionline/model/Television.java
@@ -3,12 +3,14 @@ package com.choinhet.televisionproductionline.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Television {
     @Id
     @GeneratedValue
     private Long id;
+    @NotBlank
     private String modelName;
 
     public Television() {
