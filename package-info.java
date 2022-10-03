8d82439ec94d706f3b0e4efef287ae7433d7eb3d
Rafael Choinhet <70548615+rxonhe@users.noreply.github.com>
2022-10-03 11:18:07 -0300

    adding configs

---
+++ package-info.java
@@ -0,0 +1,5 @@
+@DefaultQualifier(value = NotNull.class, locations = {TypeUseLocation.PARAMETER, TypeUseLocation.RECEIVER, TypeUseLocation.RETURN, TypeUseLocation.CONSTRUCTOR_RESULT})
+package com.choinhet.televisionproductionline;
+import org.checkerframework.framework.qual.DefaultQualifier;
+import org.checkerframework.framework.qual.TypeUseLocation;
+import org.jetbrains.annotations.NotNull;
